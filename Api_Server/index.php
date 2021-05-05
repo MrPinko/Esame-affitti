b004473640791f016e1d1e5334ecd3bedd071ec0
MrPinko <fede02rosa@gmail.com>
2021-05-05 18:50:26 +0200

    php post

--- Api_Server/index.php
+++ Api_Server/index.php
@@ -1,28 +1,32 @@
 <?php
-header("Content-Type:application/json");
-require "data.php";
-require "Users.php";
-require "DatabaseConnector.php";
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode('/', $uri);
+include "DatabaseConnector.php";
 
-// all of our endpoints start with /person
-// everything else results in a 404 Not Found
-if ($uri[2] !== 'index.php') {
-    header("HTTP/1.1 404 Not Found");
-    exit();
+$dbConnection = (new DatabaseConnector())->getConnection();
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+print_r($data);
+
+insert($data, $dbConnection);
+
+function insert(array $input, $dbConnection)
+{
+    $statement = "
+            INSERT INTO test
+                (id, testcol)
+            VALUES
+                (:id, :testcol);
+        ";
+
+    try {
+        $statement = $dbConnection->prepare($statement);
+        $statement->execute(array(
+            'id' => $input['id'],
+            'testcol' => $input['testcol'],
+        ));
+        return $statement->rowCount();
+    } catch (\PDOException$e) {
+        exit($e->getMessage());
+    }
 }
-
-$requestMethod = $_SERVER["REQUEST_METHOD"];
-
-if (isset($uri[3])) {
-    $params = $uri[3];
-} else {
-    $params = null;
-}
-
-$db = (new DatabaseConnector())->getConnection();
-
-$controller = new Users($db, $requestMethod, $params);
-$controller->processRequest();
