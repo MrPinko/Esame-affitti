74e3bb1413859c68c4fa2fb72f1744a97d15b1a5
MrPinko <fede02rosa@gmail.com>
2021-05-07 09:01:52 +0200

    tutte le richieste funzionano

---
+++ Api_Server/Controller.php
@@ -0,0 +1,147 @@
+<?php
+
+include "Database_Query.php";
+
+class Controller
+{
+    private $db;
+    private $requestMethod;
+    private $userId;
+
+    private $DBquery;
+
+    public function __construct($db, $requestMethod, $userId)
+    {
+        $this->db = $db;
+        $this->requestMethod = $requestMethod;
+        $this->userId = $userId;
+
+        $this->DBquery = new Database_Query($db);
+    }
+
+    //
+
+    public function processRequest()
+    {
+        switch ($this->requestMethod) {
+            case 'GET':
+                if ($this->userId) { //se esiste un id
+                    $response = $this->getUser($this->userId);
+                } else {
+                    $response = $this->getAllUsers();
+                };
+                break;
+            case 'POST':
+                $response = $this->createUserFromRequest();
+                break;
+            case 'PUT':
+                $response = $this->updateUserFromRequest($this->userId);
+                break;
+            case 'DELETE':
+                $response = $this->deleteUser($this->userId);
+                break;
+            default:
+                $response = $this->notFoundResponse();
+                break;
+        }
+        header($response['status_code_header']);
+        if ($response['body']) {
+            echo $response['body'];
+        }
+    }
+
+    private function getAllUsers()
+    {
+        $result = $this->DBquery->getAll();
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function getUser($id)
+    {
+        $result = $this->DBquery->find($id);
+        if (!$result) {
+            return $this->notFoundResponse();
+        }
+        echo "id = " . $result[0]['id'];
+        echo "<br>";
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function createUserFromRequest()
+    {
+        $input = (array) json_decode(file_get_contents('php://input'), true);
+        print_r($input);
+
+        /*if (!$this->validatePerson($input)) {
+            return $this->unprocessableEntityResponse();
+        }*/
+
+        $this->DBquery->insert($input);
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function updateUserFromRequest($id)
+    {
+        $result = $this->DBquery->find($id);
+
+        if (!$result) {
+            return $this->notFoundResponse();
+        }
+
+        $input = (array) json_decode(file_get_contents('php://input'), true);
+
+        /*if (!$this->validatePerson($input)) {
+            return $this->unprocessableEntityResponse();
+        }*/
+
+        $this->DBquery->update($id, $input);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function deleteUser($id)
+    {
+        $result = $this->DBquery->find($id);
+        if (!$result) {
+            return $this->notFoundResponse();
+        }
+        $this->DBquery->delete($id);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function validatePerson($input)
+    {
+        if (!isset($input['firstname'])) {
+            return false;
+        }
+        if (!isset($input['lastname'])) {
+            return false;
+        }
+        return true;
+    }
+
+    private function unprocessableEntityResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+        $response['body'] = json_encode([
+            'error' => 'Invalid input',
+        ]);
+        return $response;
+    }
+
+    private function notFoundResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = null;
+        return $response;
+    }
+}
